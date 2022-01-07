5b8636ed197085f10febffcf0093327d98aa555a
Dang Nguyen <huudang28420@gmail.com>
2022-01-07 14:03:59 +0700

    make it better

---
+++ .history/README_20211230205551.txt
@@ -0,0 +1,15 @@
+Dự án cuối kì học phần NLP
+Đề tài: Xây dựng ứng dụng website chatbot hỗ trợ, tư vấn khách hàng mua sắm
+
+Thành viên:
+1. Nguyễn Hửu Đăng - 51900784
+2. Đào Nhật Tân - 519008292
+3. Nguyễn Tấn Anh - 51900477
+
+Mô tả hệ thống:
++ Ứng dụng có 3 giao diện chính: Đăng kí, đăng nhập, Chatbot 
++ Chatbot hỗ trợ tư vấn cho khách hàng mua sắm, dẫn các đường link sản phẩm cho khách hàng
+
+Ghi chú:
++ Chạy chương trình bằng lệnh: python3 app.py
++ Link source code và demo sản phẩm (Sử dụng mail TDTU): https://drive.google.com/drive/folders/1tIyYx0Vwx4HE6c313f2iYRj_QR5Z3LTF?usp=sharing
---
+++ .history/app_20211229232050.py
@@ -0,0 +1,78 @@
+from flask import Flask, render_template,request,flash,redirect,url_for,session, request
+import sqlite3
+import train
+import chatapp
+
+app = Flask(__name__)
+app.secret_key="123"
+
+con=sqlite3.connect("database.db")
+con.execute("create table if not exists customer(pid integer primary key,name text,address text,contact integer,mail text)")
+con.close()
+
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+@app.route('/login',methods=["GET","POST"])
+def login():
+    if request.method=='POST':
+        name=request.form['name']
+        password=request.form['password']
+        con=sqlite3.connect("database.db")
+        con.row_factory=sqlite3.Row
+        cur=con.cursor()
+        cur.execute("select * from customer where name=? and password=?",(name,password))
+        data=cur.fetchone()
+
+        if data:
+            session["name"]=data["name"]
+            session["password"]=data["password"]
+            return redirect("customer")
+        else:
+            flash("Username and Password Mismatch","danger")
+    return redirect(url_for("index"))
+
+
+@app.route('/customer',methods=["GET","POST"])
+def customer():
+    train
+    return render_template("test.html")
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    if request.method=='POST':
+        try:
+            name=request.form['name']
+            mail=request.form['mail']
+            password = request.form['password']
+            fullname=request.form['fullname']
+            age = request.form['age']
+
+            con=sqlite3.connect("database.db")
+            cur=con.cursor()
+            cur.execute("insert into customer(name,mail,password,fullname,age)values(?,?,?,?,?)",(name,mail,password,fullname,age))
+            con.commit()
+            flash("Record Added  Successfully","success")
+        except:
+            flash("Error in Insert Operation","danger")
+        finally:
+            return redirect(url_for("index"))
+            con.close()
+
+    return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
+
+@app.route("/get")
+def get_bot_response():
+    chatapp
+    userText = request.args.get('msg')
+    return str(chatapp.chatbot_response(userText))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
